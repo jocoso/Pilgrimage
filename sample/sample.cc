e711f4303cd911706dfb5a3610fffac6098c5b69
Josh <11726211+jocoso@users.noreply.github.com>
2025-03-16 16:06:58 +0000

    Organized the code.
    Added a couple of useful functions like an overloaded version of "say" that takes a message and also the amount of time it will be displayed before adding another message. Also I added another wait function that takes time as a parameter if a different wait time than generic is needed.

--- sample/sample.cc
+++ sample/sample.cc
@@ -9,11 +9,10 @@ using namespace std;
 
 namespace constants 
 {
-	const std::chrono::seconds nd_pause(3); // Pause between dialogue lines. 
+	const std::chrono::seconds nd_pause(2); // Pause between dialogue lines. 
 	const char separator = ':'; 
 	const string tought_prompt = "Write a though";
-	const string tab = "====================";
-	
+	const string tab = "====================\n";
 }
 
 struct Player {
@@ -22,7 +21,9 @@ struct Player {
 
 // Prototypes
 void wait();
+void wait(const int time);
 void say(const string& message);
+void say(const string& message, const int time); 
 void looped_instruct(string message, string raw_pattern, string on_fail_msg);
 bool single_instruct(string message, string raw_pattern);
 void focus_command();
@@ -34,12 +35,22 @@ int main()
 	const string on_fail("A thought confuses you, you don't react.");
 	
 	// Introduction
-	say("You are awake...");
-	say("The sounds are veiled, yet a heartbeat echoes through.");
-	say("The image is indistinct, but shadows loom.");
-	say("The skin is fragile, yet beneath lies a gentle caress.");
-	say("Your tongue may be barren, yet flavour linger.");
-	say("Even if your nose is numb, scents still waft.");
+	say("You are awake...\n");
+	
+	say("The sounds are veiled,");
+	say("\tyet a heartbeat echoes through.");
+	
+	say("The image is indistinct,");
+	say("\tbut shadows loom.");
+	
+	say("The skin is fragile,");
+	say("\twhere beneath lies a gentle caress.");
+	
+	say("Your tongue may be barren,"); 
+	say("\tits flavour lingers?");
+	
+	say("And a numb nose,");
+	say("\tscents still waft.\n");
 	
 	// Focus command tutorial
 	say("You know you can 'focus', by merely thinking of it.");
@@ -50,7 +61,13 @@ int main()
 	// Kick command tutorial
 	say("You know you can 'kick' by merely thinking it.");
 	looped_instruct(constants::tought_prompt, R"(\b(kick)\b)", on_fail);
+	player.commands_known.push_back("kick");
 	kick_command();
+	
+	// Focus command tutorial
+	say("'focus' again...");
+	looped_instruct(constants::tought_prompt, R"(\b(focus)\b)", on_fail);
+	focus_command();
 	
 	return 0;
 }
@@ -70,8 +87,9 @@ void focus_command()
 	{
 		cout << "- " << el << endl;
 	}
+	cout << constants::tab << endl;
 	
-	while(!single_instruct("You can 'leave' wherever you choose", R"(\b(leave)\b)")) 
+	while(!single_instruct("Type 'x' to leave.\n", R"(\b(x)\b)")) 
 	{
 		cout << "== COMMANDS KNOWN ==" << endl;
 		for(string el : player.commands_known) 
@@ -122,10 +140,22 @@ void looped_instruct(string message, string raw_pattern, string on_fail_msg)
 	} while(!goal_completed);
 }
 
+void say(const string& message, const int time)
+{
+	cout << message << endl;
+	wait();
+}
+
 void say(const string& message)
 {
 	cout << message << endl;
 	wait();
+}
+
+void wait(const int time)
+{
+	std::this_thread::sleep_for(std::chrono::seconds(time));
+	
 }
 
 void wait()
